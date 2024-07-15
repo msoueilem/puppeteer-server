8696e17a6bfa8a17b97ad2c66c9cc44f059d8c3a
​Mohamed Soueilem <msoueilem@webmd.net>
2024-07-15 12:14:50 -0400

    fix api to url localhost:3000/fetch/?url

--- express-server/index.js
+++ express-server/index.js
@@ -5,7 +5,13 @@ const { validateUrl } = require('./validations/appValidations');
 const app = express();
 const PORT = 3000;
 
-app.get("/", validateUrl, fetchJavaScriptContent);
+// Route for checking server status
+app.get("/", (req, res) => {
+    res.send("Server is Successfully Running, and App is listening on port " + PORT);
+});
+
+// Route for fetching content from a URL
+app.get("/fetch/*", validateUrl, fetchJavaScriptContent);
 
 app.listen(PORT, (error) => {
     if (!error) {
--- express-server/validations/appValidations.js
+++ express-server/validations/appValidations.js
@@ -1,6 +1,11 @@
 const validateUrl = (req, res, next) => {
-    // validation logic here !
-    next();
+    const url = req.params[0];
+    // Simple validation to check if the URL starts with http:// or https://
+    if (/^https?:\/\/.*/.test(url)) {
+        next();
+    } else {
+        res.status(400).send("Invalid URL format");
+    }
 }
 
 module.exports = {
